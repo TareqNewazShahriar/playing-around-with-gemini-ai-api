03c57944f689e299e3e1d9eb9ab0f9891f40af8c
Tareq Newaz Shahriar <tareq.newaz.shahriar@gmail.com>
2024-03-06 17:19:56 +0600

    Npm pkg used. parse working. Serapate rest js.

--- main.js
+++ main.js
@@ -1,62 +1,52 @@
 require('dotenv').config()
-const request = require('request')
-//const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold, GenerativeModel } = require("@google/generative-ai");
 
-const article = `Religious people, general and scholars of religion frequently say that “science cannot figure out the ayah that are currently contradictory with scientific knowledge, but one day it will see that everything in the Quran is correct once it is much advanced”. Well, we can assume that it will be matched, also it may not be. But most importantly, this is a statement that’s basically proposing that each and every wrong statement, each and every ignorance is basically correct, until we cannot gain knowledge up to such a point that those wrongs will become right and those ignorance may become knowledge we should keep gaining knowledge. This is also true for all other religions. We didn’t gain enough knowledge to understand that those religions are correct, once we gain enough knowledge, we will see there’s nothing contradictory.
-Also telling “science cannot figure out…etc etc” is a statement that indicates that the commenter has a poor understanding of the term “science”. Science is not an organization or ideology. It is the name of a process or methodology.`
-const prompt = `Please suggest a simple title for the following article; also suggest types, categories for this article and extract keywords; out of 10, give an overall rating and rating for clarity; also do a criticism of the article, as a mini article. Do respond in markdown and respond only in valid JSON.
+const article = `If you can prove a scientific theory wrong then this is a great achievement for humans and the science community. Humans will get rid of a wrong theory. That will save huge time and money on a mistake. They will search for the correct explanation. It is your contribution to the whole of humanity. If you are pondering upon a scientific theory and ask,"if animal evolution is a fact, then where is the species that is between monkey and chimpanzee?" That is a good question to investigate. And if it is a good question to test, then test it.
+If someone has certain beliefs and he is pondering upon it to prove evolution wrong, then there's no use to engage you. Nothing but conviction to your ideology will satisfy you.`
+const prompt = `Please suggest a simple title for the following article; also suggest types, categories and extract keywords; out of 10, give an overall rating and clarity rating; also do a criticism of the article. Do NOT respond in markdown, please respond in parsable JSON.
 Here's the article:
 ${article}`
 
-// const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-// async function run() {
-//    // For text-only input, use the gemini-pro model
-//    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-   
-//    const result = await model.generateContent(prompt);
-//    const response = await result.response;
-//    const text = response.text();
-//    console.log(text);
-//    console.log(JSON.parse(text))
-// }
-// run()
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 
+// For text-only input, use the gemini-pro model
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-const payload = {
-   "contents": [{
-      "parts": [{
-         "text": prompt
-      }]
-   }]
-}
+// model.safetySettings.push({
+//    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+//    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+// })
+// model.safetySettings.push({
+//    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+//    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+// })
+// model.safetySettings.push({
+//    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+//    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+// })
+// model.safetySettings.push({
+//    category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+//    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+// })
+// model.safetySettings.push({
+//    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+//    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+// })
 
-// Replace with your API endpoint URL
-const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`;
+model.generationConfig.temperature = 0.3
 
-// Set headers with content type as JSON
-const headers = {
-   'Content-Type': 'application/json',
-};
+model.generateContent(prompt)
+   .then(result => {
+      const answerText = result.response.text();
 
-// Send the POST request
-request.post(
-   { url, headers, json: payload },
-   (error, response, body) => {
-   if (error) {
-      console.error('Error:', error);
-      return;
-   }
-   else {
-      console.log('Status:', response.statusCode);
-      const answer = body.candidates && body.candidates.length && body.candidates[0].content.parts.length ? 
-         body.candidates[0].content.parts[0].text : 
-         null
-      console.log('answer:', answer);
-
-      const jsonAnswer = answer.indexOf('{') !== -1 && answer.lastIndexOf('}') !== -1 ?
-         answer.substring(answer.indexOf('{'), answer.lastIndexOf('}')+1) : 
+      let jsonString = answerText && answerText.indexOf('{') !== -1 && answerText.lastIndexOf('}') !== -1 ?
+         answerText.substring(answerText.indexOf('{'), answerText.lastIndexOf('}') + 1) :
          null;
 
-      console.log(JSON.parse(jsonAnswer))
-   }
-});
+      jsonString = (jsonString || '').replace(/\n/g, ' ')
+      console.log(jsonString)
+      console.log(JSON.parse(jsonString))
+   })
+   .catch(error => {
+      console.error(error)
+   });
---
+++ rest-api-request.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const request = require('request')
+
+const article = `If you can prove a scientific theory wrong then this is a great achievement for humans and the science community. Humans will get rid of a wrong theory. That will save huge time and money on a mistake. They will search for the correct explanation. It is your contribution to the whole of humanity. If you are pondering upon a scientific theory and ask,"if animal evolution is a fact, then where is the species that is between monkey and chimpanzee?" That is a good question to investigate. And if it is a good question to test, then test it.
+If someone has certain beliefs and he is pondering upon it to prove evolution wrong, then there's no use to engage you. Nothing but conviction to your ideology will satisfy you.`
+const prompt = `Please suggest a simple title for the following article; also suggest types, categories and extract keywords; out of 10, give an overall rating and clarity rating; also do a criticism of the article. Do NOT respond in markdown, please respond in parsable JSON.
+Here's the article:
+${article}`
+
+const payload = {
+   "contents": [{
+      "parts": [{
+         "text": prompt
+      }]
+   }]
+}
+
+// Replace with your API endpoint URL
+const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`;
+
+// Set headers with content type as JSON
+const headers = {
+   'Content-Type': 'application/json',
+};
+
+// Send the POST request
+request.post({ url, headers, json: payload },
+   (error, response, body) => {
+      if (error) {
+         console.error('Error:', error);
+         return;
+      }
+      else {
+         console.log('Status:', response.statusCode);
+         const answerText = body.candidates && body.candidates.length && body.candidates[0].content.parts.length ?
+            body.candidates[0].content.parts[0].text :
+            null
+         console.log('answer:', answerText);
+
+         let jsonString = answerText && answerText.indexOf('{') !== -1 && answerText.lastIndexOf('}') !== -1 ?
+            answerText.substring(answerText.indexOf('{'), answerText.lastIndexOf('}') + 1) :
+            null;
+
+         jsonString = (jsonString || '').replace(/\n/g, '<br>')
+
+         console.log(JSON.parse(jsonString))
+      }
+   });
